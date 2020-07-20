4815fc8502f208dc484de670564695404bf337a4
Duncan Grubbs <duncan.grubbs@gmail.com>
2020-07-19 17:41:20 -0700

    Exploring APIs.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,7 @@
-require('location-service')
+const url1 = 'https://covidtracking.com/api/v1/us/daily.json';
+const url2 = 'https://covidtracking.com/api/v1/states/info.json';
+const url3 = 'https://covidtracking.com/api/v1/states/ca/current.json';
+
+fetch(url3)
+  .then(response => response.json())
+  .then(data => console.log(data));
